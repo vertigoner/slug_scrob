46e5b0f4137def54095676051f8eedd0d6f76cd7
vertigoner <nbroberts97@gmail.com>
2017-08-03 16:02:16 -0400

    changes to structure

--- bin/slug_scrob.sh
+++ bin/slug_scrob.sh
@@ -3,7 +3,7 @@
 echo "Welcome to Slug Scrob"
 echo
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/slug_scrob/
+cd $DIR/../slug_scrob/
 
 args=("$@")
 python3.5 ./main.py "${args[0]}" "${args[1]}"
